4f3b96a2555c11c6841f76d52034997071c243f4
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-12-18 11:25:43 +0100

    Add bouncing effect

--- CoolMenuAnimation/ContentView.swift
+++ CoolMenuAnimation/ContentView.swift
@@ -16,18 +16,25 @@ struct ContentView: View {
       Rectangle()
         .frame(width: 64, height: 10)
         .cornerRadius(4)
+        .rotationEffect(isRotating ? Angle(degrees: 34) : Angle(degrees: 0), anchor: .leading)
+
+      Rectangle()
+        .frame(width: 64, height: 10)
+        .cornerRadius(4)
+        .scaleEffect(x: isHidden ? 0 : 1,
+                     y: isHidden ? 0 : 1,
+                     anchor: .leading)
+        .opacity(isHidden ? 0 : 1)
       
       Rectangle()
         .frame(width: 64, height: 10)
         .cornerRadius(4)
-      
-      Rectangle()
-        .frame(width: 64, height: 10)
-        .cornerRadius(4)
+        .rotationEffect(isRotating ? Angle(degrees: -34) : Angle(degrees: 0), anchor: .leading)
     }
     .padding()
     .onTapGesture {
-      withAnimation {
+      // animation bounses because of .interpolatingSpring(stiffness: 300, damping: 15)
+      withAnimation(.interpolatingSpring(stiffness: 300, damping: 15)) {
         isRotating.toggle()
         isHidden.toggle()
       }
