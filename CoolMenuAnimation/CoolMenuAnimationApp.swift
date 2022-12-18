bc020a79cb4c7bed9bcfd12bf2ca7ebbf733ca47
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-12-18 11:04:51 +0100

    Initial Commit

---
+++ CoolMenuAnimation/CoolMenuAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolMenuAnimationApp.swift
+//  CoolMenuAnimation
+//
+//  Created by Kaori Persson on 2022-12-18.
+//
+
+import SwiftUI
+
+@main
+struct CoolMenuAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
